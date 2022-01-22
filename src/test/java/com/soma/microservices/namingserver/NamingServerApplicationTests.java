0bb3b160416d9087a067d1d011ad1a9e25c3f5ea
ediaz13 <diazemanuel27@gmail.com>
2022-01-22 14:39:21 -0300

    Connect currency conversion and currency exchange.

---
+++ src/test/java/com/soma/microservices/namingserver/NamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soma.microservices.namingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
